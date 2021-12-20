5e944c4b3fdba44cdc5704be93acf51e9c0dfcab
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-20 13:28:36 +0200

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,6 +44,36 @@ export function Header() {
 
         <Icon as={RiSearchLine} fontSize="20" />
       </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>José Boné</Text>
+            <Text color="gray.300" fontSize="small">
+              bjosemateus@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="José Boné"
+            src="https://github.com/jose-bone.png"
+          />
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
