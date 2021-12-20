86c73073414bc5ad94c6bddc85605a275fa410c9
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-20 14:01:18 +0200

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,13 @@
 import { Header } from "../components/Header";
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
