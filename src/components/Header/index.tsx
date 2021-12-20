2b8e6ac3e3dd9250dc29dfa8de0eec24d0ecbf6c
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-20 19:34:26 +0200

    Header responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,11 @@ import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       as="header"
@@ -19,12 +24,12 @@ export function Header() {
     >
       <Logo />
 
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
 
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
