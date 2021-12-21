40d60a37c28860f718760417eda9031b6501d5e1
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-21 21:07:51 +0200

    Form de autenticação

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -10,7 +10,10 @@ interface InputProps extends ChakraInputProps {
   label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, ...rest },
+  ref
+) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -25,8 +28,11 @@ export function Input({ name, label, ...rest }: InputProps) {
           bgColor: "gray.900",
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
