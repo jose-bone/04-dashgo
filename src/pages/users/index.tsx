d7ec493b2684e16804bdc991bb8bc019f13ed45d
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-23 17:37:34 +0200

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -26,10 +26,21 @@ export default function UserList() {
     const response = await fetch("http://localhost:3000/api/users");
     const data = await response.json();
 
-    return data;
-  });
+    const users = data.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString("pt-PT", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }),
+      };
+    });
 
-  console.log(query);
+    return users;
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -82,52 +93,24 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">José Boné</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          bjosemateus@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>20 de Dezembro de 2021</Td>}
-                  </Tr>
-                </Tbody>
-                <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">José Boné</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          bjosemateus@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>20 de Dezembro de 2021</Td>}
-                  </Tr>
-                </Tbody>
-                <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">José Boné</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          bjosemateus@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>20 de Dezembro de 2021</Td>}
-                  </Tr>
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
 
