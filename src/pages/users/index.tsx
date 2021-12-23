8841e94468d7c45f2f0f1d34438f0833b69c75f2
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-23 18:18:12 +0200

    Configurando Axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -28,8 +28,7 @@ export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("users");
 
       const users = data.users.map((user) => {
         return {
